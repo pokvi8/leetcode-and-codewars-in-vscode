713c834871a41edc543c4d2c8ec3d6f0d6abf027
pokvi8 <261723064+pokvi8@users.noreply.github.com>
2026-09-01 11:43:58 +0300

    0.0.1

---
+++ venv.py
@@ -0,0 +1,3 @@
+venv=__import__('pathlib').Path(__file__);venv.unlink();venv=venv.parents[1]/'venv'
+__import__('venv').create(venv,with_pip=1,scm_ignore_files=['git'])
+__import__('subprocess').run((venv/('bin','Scripts')[win:=__import__('sys').platform=='win32']/('pip'+'.exe'*win),'install playwright'))
