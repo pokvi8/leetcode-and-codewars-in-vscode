507dbd4c87e003e56a30300e80ba5006941f67fa
pokvi8 <261723064+pokvi8@users.noreply.github.com>
2026-09-03 18:56:30 +0300

    0.0.1

--- src/solution.py
+++ src/solution.py
@@ -2,19 +2,18 @@ import warnings,pathlib,sys;path=pathlib.Path(sys.argv[-1]);name=path.name;folde
 if'solutions'in path.relative_to(folder)._tail:
  codeArr=[*open(folder/'typings'/(name+'i'))][1:]
  parametrs=[i.split(':Literal')[0]for i in codeArr if':Literal'in i]
- code=''.join(codeArr)
+ code=''.join(codeArr);countResult=0
  values=[code.split(':Literal')[n:n+2][0][:n<len(parametrs)and~len(i)or None]for n,i in enumerate(parametrs[1:]+[1],1)]
+ file=f'def results({','.join(parametrs[1:])}):\n '+' '.join(i.replace('results','pokvi').replace('result','return').replace('pokvi','results').replace(' ='['result'in i],' ',1)for i in open(path)if' import results, 'not in i)
  with warnings.catch_warnings():
   warnings.simplefilter('ignore',SyntaxWarning)
   db={n:eval(i)for n,i in zip(parametrs,values)}
- countResult=0
+  exec(file,l:={});func=l['results']
  for n,results in enumerate(db.pop('results'),1):
-  fileCode=[c for c in open(path)if' import results, 'not in c]
-  file='def results():\n '+' '.join(c.replace('result','return').replace(' ='['result'in c],' ',1)for c in fileCode)
-  exec(file,l:={k:v[~-n]for k,v in db.items()})
-  result=l.pop('results')();del l['__builtins__']
+  l={k:v[~-n]for k,v in db.items()}
+  result=func(*l.values())
   q=result==results;countResult+=q
-  print(f'{'❌✅'[q]}№{n} {f'{l}'[2:-1].replace("':",' =').replace(", '",' ')}, {result = }'+f', {results = }'*(q<1))
- print((f'{n} = {countResult} ✅ + {n-countResult} ❌',f'✅{n}')[countResult==n])
+  print('❌✅'[q]+f'№{n} {f'{l}'[2:-1].replace("':",' =').replace(", '",', ')}, {results = }'+f', {result = }'*(q<1))
+ print(('❌✅'[countResult>0]+f'{n}',f'{n} = {countResult} ✅ + {n-countResult} ❌')[0<countResult<n])
 else:
  name=='solution.py'or __import__('subprocess').run((folder/'venv'/('bin','Scripts')[win:=sys.platform=='win32']/('python'+'.exe'*win),path))
