8a7906aa392e65dd33a07e6d30bc60b82170d6c5
pokvi8 <261723064+pokvi8@users.noreply.github.com>
2026-09-17 12:56:30 +0300

    0.0.1

--- tests/codewars_test.py
+++ tests/codewars_test.py
@@ -7,7 +7,7 @@ def solutions(*_):
  code='\n'.join(f'{(a:=i.split('[',1))[0]}[{a[1].strip('\n,')[:-1]},{repr(_[parameters.index(a[0]+'[]')])}]'for i in(pyi.exists()and open(pyi)or parameters)if':Literal['in i)
  pyi.write_text('from typing import Literal\n'+code)
 
-describe=lambda _:(lambda _:_())
-it=lambda _:(lambda _:_())
+describe=lambda _:lambda _:_()
+it=lambda _:lambda _:_()
 assert_equals=lambda result,results,_=None:solutions(results,*result['pokvi'])
 expect=lambda result,_=None:solutions(*(result[a:=(dict==type(s:=result[0])and'pokvi'in s)*2],*result[~a]['pokvi']))
